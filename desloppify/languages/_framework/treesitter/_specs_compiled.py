b66f46b1e47c741b5603ea2167a75b435375bc13
POM <peter@omalley.io>
2026-03-09 02:30:00 +0100

    desloppify: split native treesitter compiled specs

--- desloppify/languages/_framework/treesitter/_specs_compiled.py
+++ desloppify/languages/_framework/treesitter/_specs_compiled.py
@@ -6,15 +6,14 @@ from desloppify.languages._framework.treesitter import TreeSitterLangSpec
 
 from ._import_resolvers_backend import (
     resolve_csharp_import,
-    resolve_cxx_include,
     resolve_dart_import,
     resolve_go_import,
     resolve_java_import,
     resolve_kotlin_import,
     resolve_rust_import,
-    resolve_scala_import,
 )
 from ._import_resolvers_scripts import resolve_php_import
+from ._specs_compiled_native import C_SPEC, CPP_SPEC, SCALA_SPEC
 
 GO_SPEC = TreeSitterLangSpec(
     grammar="go",
@@ -233,89 +232,5 @@ DART_SPEC = TreeSitterLangSpec(
     """,
     log_patterns=(
         r"^\s*(?:print\(|debugPrint|log\.)",
-    ),
-)
-
-C_SPEC = TreeSitterLangSpec(
-    grammar="c",
-    function_query="""
-        (function_definition
-            declarator: (function_declarator
-                declarator: (identifier) @name)
-            body: (compound_statement) @body) @func
-    """,
-    comment_node_types=frozenset({"comment"}),
-    import_query="""
-        (preproc_include
-            path: (string_literal) @path) @import
-    """,
-    resolve_import=resolve_cxx_include,
-    class_query="""
-        (struct_specifier
-            name: (type_identifier) @name
-            body: (field_declaration_list) @body) @class
-    """,
-    log_patterns=(
-        r"^\s*(?:printf\(|fprintf\(|perror\()",
-    ),
-)
-
-CPP_SPEC = TreeSitterLangSpec(
-    grammar="cpp",
-    function_query="""
-        (function_definition
-            declarator: (function_declarator
-                declarator: (identifier) @name)
-            body: (compound_statement) @body) @func
-        (function_definition
-            declarator: (function_declarator
-                declarator: (qualified_identifier) @name)
-            body: (compound_statement) @body) @func
-    """,
-    comment_node_types=frozenset({"comment"}),
-    import_query="""
-        (preproc_include
-            path: (string_literal) @path) @import
-    """,
-    resolve_import=resolve_cxx_include,
-    class_query="""
-        (class_specifier
-            name: (type_identifier) @name
-            body: (field_declaration_list) @body) @class
-        (struct_specifier
-            name: (type_identifier) @name
-            body: (field_declaration_list) @body) @class
-    """,
-    log_patterns=(
-        r"^\s*(?:std::cout|std::cerr|printf\(|fprintf\()",
-    ),
-)
-
-SCALA_SPEC = TreeSitterLangSpec(
-    grammar="scala",
-    function_query="""
-        (function_definition
-            name: (identifier) @name
-            body: (_) @body) @func
-    """,
-    comment_node_types=frozenset({"comment", "block_comment"}),
-    import_query="""
-        (import_declaration
-            path: (identifier) @path) @import
-    """,
-    resolve_import=resolve_scala_import,
-    class_query="""
-        (class_definition
-            name: (identifier) @name
-            body: (template_body) @body) @class
-        (object_definition
-            name: (identifier) @name
-            body: (template_body) @body) @class
-        (trait_definition
-            name: (identifier) @name
-            body: (template_body) @body) @class
-    """,
-    log_patterns=(
-        r"^\s*(?:println\(|print\(|Logger\.|log\.)",
     ),
 )
